2655c3491b5e1d56f869dc9c125311160d2c7153
RajadorDev <rajadordeveloper@gmail.com>
2026-05-08 21:09:08 -0300

    Added system util

---
+++ src/rajadordev/skinutils/utils/SystemUtils.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/***
+ *   
+ * Rajador Developer Diamond API
+ * 
+ *  ██████╗  █████╗      ██╗ █████╗ ██████╗  ██████╗ ██████╗ 
+ *  ██╔══██╗██╔══██╗     ██║██╔══██╗██╔══██╗██╔═══██╗██╔══██╗
+ *  ██████╔╝███████║     ██║███████║██║  ██║██║   ██║██████╔╝
+ *  ██╔══██╗██╔══██║██   ██║██╔══██║██║  ██║██║   ██║██╔══██╗
+ *  ██║  ██║██║  ██║╚█████╔╝██║  ██║██████╔╝╚██████╔╝██║  ██║
+    ╚═╝  ╚═╝╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
+ * 
+ * GitHub: https://github.com/rajadordev
+ * 
+ * Discord: rajadortv
+ * 
+ * @copyright 2023 - 2027 Rajador Developer
+ * 
+ * Repository: https://github.com/RajadorDev/SkinUtilsLib
+ * 
+ **/
+
+namespace rajadordev\skinutils\utils;
+
+use pocketmine\Player;
+
+final class SystemUtils 
+{
+
+    public static function isValisPlayer($player) : bool 
+    {
+        return ($player instanceof Player && $player->isOnline());
+    }
+}
